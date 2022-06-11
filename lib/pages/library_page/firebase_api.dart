1f06b80b857aedf88e0c0fbfc7f94cf075e1f2b0
Dastronaut <trankimtiendatip@gmail.com>
2022-06-11 11:22:59 +0700

    m

--- lib/pages/library_page/firebase_api.dart
+++ lib/pages/library_page/firebase_api.dart
@@ -1,15 +1,11 @@
-import 'package:flutter/material.dart';
-
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:s_camera/pages/library_page/firebase_file.dart';
-import 'package:path_provider/path_provider.dart';
 
-class FirebaseApi{
+class FirebaseApi {
   static Future<List<String>> _getDownLoadLinks(List<Reference> refs) =>
       Future.wait(refs.map((ref) => ref.getDownloadURL()).toList());
 
-
-  static Future<List<FirebaseFile>> listAll(String path) async{
+  static Future<List<FirebaseFile>> listAll(String path) async {
     final ref = FirebaseStorage.instance.ref(path);
     final result = await ref.listAll();
 
@@ -18,12 +14,12 @@ class FirebaseApi{
     return urls
         .asMap()
         .map((index, url) {
-      final ref = result.items[index];
-      final name = ref.name;
-      final file = FirebaseFile(ref: ref, name: name, url: url);
+          final ref = result.items[index];
+          final name = ref.name;
+          final file = FirebaseFile(ref: ref, name: name, url: url);
 
-      return MapEntry(index, file);
-    })
+          return MapEntry(index, file);
+        })
         .values
         .toList();
   }
